36dd1dec2e320c28e20f726f65d5999929fe4756
JoelWi <joel@joelwi.dev>
2023-12-22 21:39:15 +0900

    conditional cases setup

--- tests/conditional.rs
+++ tests/conditional.rs
@@ -5,12 +5,19 @@ fn condition_present_with_no_valid_param_provided() {
     let html = "Hello{#if first_name?? && first_name?not_empty#}${first_name}{#endif#}!".to_owned();
     let mut params = HashMap::new();
 
-    params.insert(
-        "first_name".to_owned(),
-        serde_json::json!({
-            "first_name": "Joel"
-        }),
-    );
+    params.insert("first_name".to_owned(), serde_json::json!({}));
+
+    let expeccted_output = "Hello!".to_owned();
+    let rendered_html = kitamura::render_template(html, params).unwrap();
+    assert_eq!(rendered_html, expeccted_output);
+}
+
+#[test]
+fn nested_condition_present_with_no_valid_param_provided() {
+    let html = "Hello{#if first_name?? && first_name?not_empty#}${first_name}{#if last_name?? && last_name?not_empty#}${last_name}{#endif#}{#endif#}!".to_owned();
+    let mut params = HashMap::new();
+
+    params.insert("first_name".to_owned(), serde_json::json!({}));
 
     let expeccted_output = "Hello!".to_owned();
     let rendered_html = kitamura::render_template(html, params).unwrap();
